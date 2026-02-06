d48735a2f562e677dd7af79590decd463ce7cad5
MD_Kayesur <md_kayesur@bdcallings-Mac-mini-5.local>
2026-02-06 12:33:47 +0600

    done  impelemtn subscribesion  in graphchart

--- src/app/(withAdminLayout)/admin/dashboard/page.tsx
+++ src/app/(withAdminLayout)/admin/dashboard/page.tsx
@@ -4,18 +4,23 @@ import CalculateCard from "@/components/reuseabelComponents/calculateCard";
 import Title from "@/components/reuseabelComponents/Title";
 import Wrapper from "@/components/wrapper/wrapper";
 import WeeklyChart from "@/components/reuseabelComponents/WeeklyChart";
-
+import SubscriptionChart from "@/components/Dashboard/SubscriptionChart";
+ 
 const DashboardPage = () => {
   return (
     <Wrapper>
       <div className="w-full  min-h-screen space-y-4 ">
         <Title title="Admin Dashboard" />
         <div className="space-y-5">
+          <SubscriptionChart period="weekly" />
+        </div>
+        <div className="space-y-5">
           <CalculateCard />
         </div>
         <div className="space-y-5">
           <WeeklyChart />
         </div>
+       
       </div>
     </Wrapper>
   );
